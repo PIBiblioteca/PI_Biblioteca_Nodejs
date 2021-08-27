185c858acdd6d8ec2ed5a88baf6d081128f0cc57
JuanTeixeira <88690110+JuanTeixeira@users.noreply.github.com>
2021-08-27 17:16:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fatecApi = require('fatec-api')
-const minhaConta = new fatecApi.Account('628281699SP', '41350925861')
+const minhaConta = new fatecApi.Account('', '')
  
 minhaConta.getProfile().then(nome => {
     console.log(nome)

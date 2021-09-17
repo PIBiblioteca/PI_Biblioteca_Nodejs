36f22bf6e4dc0220c6d8508aa836989bf1dc7f5c
JuanTeixeira <88690110+JuanTeixeira@users.noreply.github.com>
2021-09-17 18:48:51 -0300

    Console aparecer no Browser
    Tentativa de fazer com que pelo menos a lista de livros aparece no console do navegador, porém não funcionou. Depois de procurar a razão, consegui descobrir que o Browser não faz a interpretação do <(require)> dos módulos instalados no Node.js Apenas o próprio console do Node faz essa interpretação.

--- front/index.js
+++ front/index.js
@@ -1,6 +1,6 @@
 function name(params) {
     $.ajax({
-        type: "GET",
+        type:"GET",
         url: "http://localhost:3000/login",
         data: {"usuario":"06392192255SP","senha":"8583ZDFmGyPMWez"},
         success: function (data) {
